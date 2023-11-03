f142dcaa93305a589ac755c987dc7a3d326a74c8
Zoltán Kovács <zoltan@geogebra.org>
2023-11-03 18:38:55 +0100

    Fine tune gettext

--- gettext/4-create-resources.sh
+++ gettext/4-create-resources.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -x
 
-# This scripts creates all ResourceBundle class files in the package jgex.i18n.
+# This scripts creates all ResourceBundle class files in the package i18n. It creates ../classes/i18n/Messages*.class,
+# therefore, before or after a successful Java build this script should also be called (before starting the application)
+# to put these files in the .jar bundle as well.
+#
+# That is, the following scenario should work: "cd ..; ant clean init; cd gettext; ./4-create-resources.sh; cd ..; ant all"
+#
 # See https://xnap-commons.sourceforge.net/gettext-commons/tutorial.html for details.
 
 set -e
@@ -8,5 +13,5 @@ set -e
 . settings.conf
 
 for i in $SUPPORTED_LANGUAGES; do
- msgfmt --java2 -d ../classes -r wprover.Messages -l $i po/$i.po
+ msgfmt --java2 -d ../classes -r i18n.Messages -l $i po/$i.po
  done
