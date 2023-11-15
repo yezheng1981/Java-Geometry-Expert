3cc25519442e070af08ee50be0f334dec8119e63
Zoltán Kovács <zoltan@geogebra.org>
2023-11-16 00:01:19 +0100

    Fix ambiguous translations by choosing the first option automatically

--- gettext/2.5-merge-old-translations.sh
+++ gettext/2.5-merge-old-translations.sh
@@ -21,7 +21,7 @@ for i in $SUPPORTED_LANGUAGES; do
  cat ../language/$l.lan | dos2unix | grep -v ^Font | grep '#' | sed s/"\""/"\\\\\""/g | \
   awk -F '#' '{gsub(/^[ \t]+|[ \t]+$/,"", $2); gsub(/^[ \t]+|[ \t]+$/,"", $3); print "msgid \"" $2 "\""; print "msgstr \"" $3 "\""; print ""}' > po/old-$i.po
  cat po/$i.po po/old-$i.po > po/$i-merged.po
- msguniq po/$i-merged.po -o po/$i.po
+ msguniq --use-first po/$i-merged.po -o po/$i.po
  rm po/$i-merged.po
 
  done
